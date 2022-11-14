9e43095e35af413f6f5b82b2746c4f6aec336a64
Hsu <jlhsu@>
2022-11-14 20:27:10 +0800

    revised

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from 'https://cms.cdn.91app.com/images/original/2454/a35f7f73-7680-41ea-afe1-ae4ae9100309-1665975215-eb0pye8eeb_m_1200x1750_800x1167_400x583.jpg';
 import './App.css';
 
 function App() {
